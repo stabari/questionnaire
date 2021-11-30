177589abd65f6e99a21a75151998209472698dd8
Jai Balaji Sukumar <jai_sbalaji@yahoo.co.in>
2021-11-30 00:23:53 -0500

    Added data files

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, json, Response, jsonify
 import pickle
 
 from flask_cors import CORS, cross_origin
+import pandas as pd
 # initialized the Flask APP
 app = Flask(__name__)
 CORS(app, support_credentials=True)
@@ -10,23 +11,28 @@ min = [-4.52364,-4.53089,	-4.83648,	-1.24421,	0.0000717008,	0.12051,	-0.05765,	0
 max= [-2.59793,	-2.58566,	-1.77495,	1.37953,	0.070945104,	1.20653,	0.05116,	0.163,	0.1482,	0.1523,	0.0455,	0.0338,	0.0737,	0.05882
 ]
 
-# Fetchs the MongoDB, by making use of Request Body
+# Fetchs the model file for prediction
 model1 = pickle.load(open('model.pkl','rb'))
 
 def normalize_input(input):
-    
-  
+    """
+    Normalizes the given input values min/max.
+    """
     r = []
     i=0;
     for x in input:
-        t = (x - min[i])/(max[i]-min[i])
+        t = 0 # modify code here
         r.append(t)
         i=i+1
     return r;
 
 # Achieving CRUD through API - '/crudapi'
-@app.route('/predict', methods=['POST'])    
+@app.route('/predict', methods=['POST'])
 def read_data():
+    """
+    Takes in a list of values from the user and normalizes it and makes prediction with it.
+    """
+
     data = request.json
     read_obj = normalize_input(data)
     print([read_obj])
@@ -36,8 +42,16 @@ def read_data():
    
 @app.route('/', methods=['GET'])
 def api():
+    """
+    Tests whether the API is active.
+    """
     return "API is active"
 
+@app.route('/data', methods=['GET'])
+def data():
+    live_df = pd.read_excel('data/SPX_live0.xlsx')
+    print(live_df)
+    return live_df.to_json()
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
